de24e6d191cb036651c1fdc42a60e6ab95b6e09b
dileep-manchikanti <60563647+dileep-manchikanti@users.noreply.github.com>
2020-09-16 19:53:39 +0530

    Update Array-Prime.c

--- Array-Prime.c
+++ Array-Prime.c
@@ -3,10 +3,11 @@ int main(){
 int A[]={7,11,13,15,19};
 int n=5,isPrime=1,i,j;
 for(i=0;i<n;i++,isPrime=1){
-    //To check if A[i]
+    is_prime=1;
     for(j=2;j<A[i];j++)
         if(A[i]%j==0)
             isPrime=0;
+            break;
     if(isPrime)
         printf("%d ",A[i]);
 }
